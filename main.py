663f885f9ce560b2907cef9d0e1d31ad4eeab0b7
AlejandroGuinel <alejandroguinel@hotmail.com>
2026-03-05 01:08:43 +0000

    primera prueba de python

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+print("hola mundo")
+
+
+# variables
+nombre= "alejandro"
+edad= 32
+es_profesor= False
+cuidad= "osorno"
+genero= ["hombre","mujer","therian"]
+
+#tipos de datos 
+
+
+
+texto = input()
+print(texto)
