cf5bff10acc91b4dbaf6815a61256e04b79bbad1
AlejandroGuinel <alejandroguinel@hotmail.com>
2026-03-09 23:49:55 +0000

    leccion1 modulo 3

---
+++ actividad_m3_l1/primeros-pasos.py
@@ -0,0 +1,24 @@
+
+print("¡Hola mundo!.")
+
+nombre = "Alejandro"   
+edad = 31              
+ciudad = "Osorno"      
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
+
+
+numero1 = 10
+numero2 = 5
+
+resultado = numero1 + numero2
+
+print("La suma de los números es:", resultado)
+
+import math
+
+raiz = math.sqrt(25)
+
+print("La raíz cuadrada de 25 es:", raiz)
